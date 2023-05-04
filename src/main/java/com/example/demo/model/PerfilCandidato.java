01b97d8d828cada02a4260c42b802b3cc7c80cd8
cariinee <esttev@gmail.com>
2023-05-03 21:59:24 -0300

    first commit

---
+++ src/main/java/com/example/demo/model/PerfilCandidato.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class PerfilCandidato {
+    @Id
+    private Integer id;
+}
---
+++ src/main/java/com/example/demo/model/Tecnologia.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Tecnologia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 200, nullable = false)
+    private String nome;
+    @Column(length = 200, nullable = false)
+    private Integer nivel;
+}
